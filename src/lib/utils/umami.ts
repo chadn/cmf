497a4575feb39fb507b8e449a3540ef105d6f2a4
Chad Norwood <chad@chadnorwood.com>
2025-05-01 11:37:20 -0700

    more tweaks for umami domain

--- src/lib/utils/umami.ts
+++ src/lib/utils/umami.ts
@@ -17,7 +17,10 @@ const sessionIds = {
     'cmf-chad.vercel.app': '5b4eb79f-b7a7-4bea-b03b-808126201cb0',
     'cmf-dev.vercel.app': '74a92658-839d-4055-9cea-4647fe94896c',
 }
-const domain = process.env.NEXT_PUBLIC_VERCEL_URL || 'cmf-chad.vercel.app'
-export const umamiWebsiteId = sessionIds[domain as keyof typeof sessionIds]
+// ex: NEXT_PUBLIC_VERCEL_URL=cmf-h9udq7nfi-all-chads.vercel.app for cmf-dev.vercel.app
+const domain = process.env.VERCEL_PROJECT_PRODUCTION_URL
+export const umamiWebsiteId = sessionIds[domain as keyof typeof sessionIds] || sessionIds['cmf-chad.vercel.app']
 
-logr.info('umami', `init for ${domain} ${umamiWebsiteId}`)
+// just get the first 8 characters of the sha
+const sha = (process.env.VERCEL_GIT_COMMIT_SHA || 'xxxxxxxxxx').substring(0, 8)
+logr.info('umami', `init for ${domain} ${umamiWebsiteId} ${sha}`)
